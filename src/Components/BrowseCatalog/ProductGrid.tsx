d547c83afcc7d5ae75dac1ac27ccf804443ba530
Ralph R <rrouhana@edu.uwaterloo.ca>
2020-11-22 19:32:15 -0500

    added loader for api call on browse-catalog page

--- src/Components/BrowseCatalog/ProductGrid.tsx
+++ src/Components/BrowseCatalog/ProductGrid.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 
 // Components
 import Product from '../Product';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 interface ProductGridProps {
     onClickAddToCart: (src: string, description: string, price: number) => void;
@@ -11,11 +12,20 @@ interface ProductGridProps {
 const ProductGrid: React.FC<ProductGridProps> = (props) => {
     let { demographic, productType } = useParams();
     const [productData, setProductData] = useState<{_id: string, description: string, src: string, price: number, widthCont: string, widthImage: string, widthButton: string, contClass: string, contId: string, demographic: string, productType: string}[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    const loadData = async () => {
+        const response = await fetch(`https://wicked-phantom-05767.herokuapp.com/https://clothing-site-backend.herokuapp.com/${productType}`);
+        const json = await response.json();
+        setProductData(json);
+        setIsLoading(false);
+    }
 
     useEffect(() => {
-        fetch(`https://wicked-phantom-05767.herokuapp.com/https://clothing-site-backend.herokuapp.com/${productType}`)
-            .then(res => res.json())
-            .then(data => setProductData(data));
+        loadData();
+        // fetch(`https://wicked-phantom-05767.herokuapp.com/https://clothing-site-backend.herokuapp.com/${productType}`)
+        //     .then(res => res.json())
+        //     .then(data => setProductData(data));
     }, [productType]);
 
     return (
@@ -25,6 +35,12 @@ const ProductGrid: React.FC<ProductGridProps> = (props) => {
                 <span>This site is a non-public demo, these are not for sale. All clothing and images belong to Guess.</span>
             </div>
             <div className="wrap flex mobile-center">
+            {isLoading ? 
+                <div className="center column loader">
+                    <CircularProgress className="loader-icon" />
+                    <span className="loader-text">Free hosting is slow... Give it a minute!</span>
+                </div> 
+                : ""}
                 {productData.map((product, index) => {
                     return (
                         <Product 
