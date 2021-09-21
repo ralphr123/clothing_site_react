b5253b37bfd0337b36bf5cd52ce96226a5836bd7
Ralph Rouhana <rrouhana@uwaterloo.ca>
2021-09-21 17:27:03 -0400

    Updated resume link

--- src/Components/Popups/About.tsx
+++ src/Components/Popups/About.tsx
@@ -14,7 +14,7 @@ interface AboutProps {
 const About: React.FC<AboutProps> = (props) => {
     const handleOnClick = (option: boolean) => {
         if (option) window.open("https://github.com/ralphr123/project4_clothing_site_react",'_blank');
-        else window.open("https://drive.google.com/open?id=1bTmV2UIPYsFqlGGiW2WdLHdOePzXUSfD");
+        else window.open("https://drive.google.com/file/d/1Sn2Dn6_wZ0JCTcACxmCwODR3npRRArHO/view?usp=sharing");
     }
     return (
         <div className="about center-text">
